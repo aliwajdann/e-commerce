c07705987a2351e3e1c9a31084ff98ce2473fc69
Ali Wajdan <aliwajdan.it@gmail.com>
2025-06-03 18:27:56 +0500

    commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,70 +1,37 @@
-'use client';
+// app/category/[id]/page.tsx
+export const dynamic = "force-dynamic";
 
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
-import { doc, getDoc } from 'firebase/firestore';
-import { useDispatch } from 'react-redux';
-import { db } from '@/lib/firebase';
-import RelatedProducts from '@/components/RelatedProducts'
-
-
-import ProductMedia from '@/components/productPage/ProductMedia';
-import ProductInfo from '@/components/productPage/ProductInfo';
-
-interface MediaType {
-  type: 'image' | 'video';
-  url: string;
-}
+import { db } from "@/lib/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import CategoryPageClient from "@/components/CategoryPageClient";
 
 interface Product {
   id: string;
   title: string;
   price: number;
   originalPrice: number;
-  description: string;
-  media: MediaType[];
+  media: string[];
 }
 
-export default function ProductPage() {
-  // const dispatch = useDispatch();
-  const params = useParams();
-  const id = params?.id as string;
+interface CategoryPageProps {
+  params: {
+    id: string;
+  };
+}
 
-  const [product, setProduct] = useState<Product | null>(null);
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const category = params.id;
 
-  useEffect(() => {
-    if (!id) return;
+  if (!category) {
+    throw new Error("Category slug is missing!");
+  }
 
-    const fetchProduct = async () => {
-      const docRef = doc(db, 'products', id);
-      const docSnap = await getDoc(docRef);
+  const q = query(collection(db, "products"), where("category", "==", category));
+  const snapshot = await getDocs(q);
+  const products: Product[] = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as any),
+  }));
 
-      if (docSnap.exists()) {
-        setProduct({ ...docSnap.data(), id: docSnap.id } as Product);
-      } else {
-        console.error('❌ Product not found');
-      }
-    };
-
-    fetchProduct();
-  }, [id]);
-
-  if (!product) return <p className="p-8">Loading...</p>;
-
-  return (
-    <>
-    <div className="p-6 md:p-10 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-6xl mx-auto mt-12">
-      <ProductMedia media={product.media} />
-      <ProductInfo
-        id={product.id}
-        title={product.title}
-        price={product.price}
-        description={product.description}
-        media={product.media}
-        originalprice={product.originalPrice}
-      />
-    </div>
-    <RelatedProducts/>
-</>
-  );
+  return <CategoryPageClient category={category} products={products} />;
 }
