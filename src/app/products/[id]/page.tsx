ea4e6b55d1103b068b0d8c0b75ee6a4cbb6bca9d
Ali Wajdan <aliwajdan.it@gmail.com>
2025-06-03 18:43:21 +0500

    commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,43 +2,69 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
+import { useDispatch } from 'react-redux';
 import { db } from '@/lib/firebase';
-import CategoryPageClient from '@/components/CategoryPageClient';
+import RelatedProducts from '@/components/RelatedProducts'
+
+
+import ProductMedia from '@/components/productPage/ProductMedia';
+import ProductInfo from '@/components/productPage/ProductInfo';
+
+interface MediaType {
+  type: 'image' | 'video';
+  url: string;
+}
 
 interface Product {
   id: string;
   title: string;
   price: number;
   originalPrice: number;
-  media: string[];
+  description: string;
+  media: MediaType[];
 }
 
-export default function CategoryPage() {
-  const { id } = useParams();
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
+export default function ProductPage() {
+  // const dispatch = useDispatch();
+  const params = useParams();
+  const id = params?.id as string;
+
+  const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
     if (!id) return;
 
-    const fetchProducts = async () => {
-      const q = query(collection(db, 'products'), where('category', '==', id));
-      const snapshot = await getDocs(q);
+    const fetchProduct = async () => {
+      const docRef = doc(db, 'products', id);
+      const docSnap = await getDoc(docRef);
 
-      const productsData: Product[] = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...(doc.data() as any),
-      }));
-
-      setProducts(productsData);
-      setLoading(false);
+      if (docSnap.exists()) {
+        setProduct({ ...docSnap.data(), id: docSnap.id } as Product);
+      } else {
+        console.error('❌ Product not found');
+      }
     };
 
-    fetchProducts();
+    fetchProduct();
   }, [id]);
 
-  if (loading) return <p className="p-6">Loading products...</p>;
+  if (!product) return <p className="p-8">Loading...</p>;
 
-  return <CategoryPageClient category={id as string} products={products} />;
+  return (
+    <>
+    <div className="p-6 md:p-10 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-6xl mx-auto mt-12">
+      <ProductMedia media={product.media} />
+      <ProductInfo
+        id={product.id}
+        title={product.title}
+        price={product.price}
+        description={product.description}
+        media={product.media}
+        originalprice={product.originalPrice}
+      />
+    </div>
+    <RelatedProducts/>
+</>
+  );
 }
