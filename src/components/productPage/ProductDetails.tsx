b4f0d3752a283dde683570f97d135e2ceb79a654
Ali Wajdan <aliwajdan.it@gmail.com>
2025-08-25 17:17:01 +0500

    commit

--- src/components/productPage/ProductDetails.tsx
+++ src/components/productPage/ProductDetails.tsx
@@ -48,13 +48,13 @@ export default function ProductDetails({ productdetails }: ProductDetailsProps) 
       label: "Returns",
       content: <>
        <div className="max-w-4xl mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold mb-4">Return & Refund Policy</h1>
-      <ul className="list-disc pl-5 space-y-3 text-gray-700 text-lg">
-        <li>You can return the product within 7 days of receiving it.</li>
-        <li>The item must be unused, with tags, and in original packaging.</li>
-        <li>If there’s a defect or issue, contact us via WhatsApp for resolution.</li>
-        <li>Refunds are processed within 5–7 business days after we receive the item.</li>
-        <li>Return shipping will be handled by us in case of damaged/wrong product.</li>
+      <h1 className="text-md font-bold mb-4">Return & Refund Policy</h1>
+      <ul className="text-sm list-disc pl-5 space-y-3 text-gray-700 ">
+        <li className="text-xs">You can return the product within 7 days of receiving it.</li>
+        <li className="text-xs">The item must be unused, with tags, and in original packaging.</li>
+        <li className="text-xs">If there’s a defect or issue, contact us via WhatsApp for resolution.</li>
+        <li className="text-xs">Refunds are processed within 5–7 business days after we receive the item.</li>
+        <li className="text-xs">Return shipping will be handled by us in case of damaged/wrong product.</li>
       </ul>
     </div>
        </>,
